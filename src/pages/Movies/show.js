098f9ace76c631a14df857d93d053e14ecea987c
Rafijohari_ <rafi.johari@widyatama.ac.id>
2023-05-20 12:03:17 +0700

    init project

---
+++ src/pages/Movies/show.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import MovieDetail from '../../components/movies/MovieDetail';
+
+const ShowMovie = () => {
+    let { id } = useParams();
+    // init data
+	const [movie, setMovie] = useState([]);
+	const [loaded, setLoaded] = useState(false)
+	const [errorMessage, setErrorMessage] = useState(null)
+
+    // komponen ini di panggil ketika component pertama kali di panggil
+	useEffect(() => {
+		const fetchMovies = async () => {
+			try {
+				const result = await axios(`http://localhost:4000/movies/${id}`);
+				await setMovie(result.data.movie);
+				setLoaded(true);
+			} catch (err) {
+				setErrorMessage(err.response.data)
+			}
+		};
+		fetchMovies();
+	}, []);
+
+    return (
+    <>
+        {!loaded ? (
+            (() => {
+                if (errorMessage) {
+                    return (
+                        <div className='row'>
+                            <p>Oops... {errorMessage}</p>
+                        </div>
+                    );
+                } else {
+                    return (
+                        <div className='row'>
+                            <p>Loading...</p>
+                        </div>
+                    );
+                }
+            })()
+        ) : (
+            <MovieDetail movie={movie} />
+        )}
+    </>
+    )
+}
+
+export default ShowMovie
